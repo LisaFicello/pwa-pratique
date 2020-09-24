7c215de0c8d023190281d6a3c31592889c4ad65e
Lisa Figarelli <l.figarelli@jellysmack.com>
2020-09-24 14:44:44 +0200

    Change sw.js file

--- sw.js
+++ sw.js
@@ -1,27 +1,5 @@
 console.log("hello depuis le service worker");
 
-self.addEventListener('install', (evt) => {
-    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
-});
-
-self.addEventListener('activate', (evt) => {
-    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
-});
-
-self.addEventListener('fetch', (evt) => {
-    console.log('sw intercepte la requête suivante via fetch', evt);
-    console.log('url interceptée', evt.request.url);
-});
-
-self.addEventListener('fetch', (evt) => {
-    if(!navigator.onLine) {
-        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
-        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
-    }
-    console.log('sw intercepte la requête suivante via fetch', evt);
-    console.log('url interceptée', evt.request.url);
-});
-
 const cacheName = 'veille-techno' + '1.1';
 
 self.addEventListener('install', (evt) => {
@@ -43,7 +21,22 @@ self.addEventListener('install', (evt) => {
     });
 
     evt.waitUntil(cachePromise);
-
 });
 
-//...
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('sw intercepte la requête suivante via fetch', evt);
+    console.log('url interceptée', evt.request.url);
+});
+
+self.addEventListener('fetch', (evt) => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
+    }
+    console.log('sw intercepte la requête suivante via fetch', evt);
+    console.log('url interceptée', evt.request.url);
+});
