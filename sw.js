b1c050300fc9426164ac6cc6faca071841b391e6
Lisa Figarelli <l.figarelli@jellysmack.com>
2020-09-24 14:55:24 +0200

    4.4 Gestion du cache par le SW : résultat

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-console.log("hello depuis le service worker");
-
 const cacheName = 'veille-techno' + '1.1';
-
+	
 self.addEventListener('install', (evt) => {
     console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
-
     const cachePromise = caches.open(cacheName).then(cache => {
+	
         return cache.addAll([
             'index.html',
             'main.js',
@@ -13,14 +11,25 @@ self.addEventListener('install', (evt) => {
             'vendors/bootstrap4.min.css',
             'add_techno.html',
             'add_techno.js',
+	
             'contact.html',
+	
             'contact.js',
+	
         ])
+	
         .then(console.log('cache initialisé'))
+	
         .catch(console.err);
+	
     });
-
+	
+ 
+	
     evt.waitUntil(cachePromise);
+	
+ 
+	
 });
 
 self.addEventListener('activate', (evt) => {
